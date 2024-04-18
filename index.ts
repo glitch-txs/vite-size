b87e04f3bc562e66afab896beae97aed402127ef
Glitch <glitch.txs@gmail.com>
2024-04-18 01:34:13 -0300

    use Vite

--- index.ts
+++ index.ts
@@ -1,23 +1,55 @@
-import { build, filesSizeStore } from '@glitch-txs/vite'
+import { build, type InlineConfig } from 'vite'
+import { promisify } from 'node:util'
+import zlib from 'node:zlib'
 
-filesSizeStore.sub.filesSize((file)=>{
-  console.log("file",file)
-})
+const gzip = promisify(zlib.gzip)
 
-const res = await build({
-  root: './dist',
-  build: {
-    minify: true,
-    outDir: './distVite',
-    lib: {
-      entry: 'test.js',
-      formats: ['es']
+interface FileInfo {
+  name: string
+  size: string | number
+  gzip: string | number
+}
+
+export async function buildVite(inlineConfig?:InlineConfig){
+  
+  const filesInfo: FileInfo[] = []
+
+  type Return = Awaited<ReturnType<typeof build>>
+
+  const res: Omit<Return, 'RollupWatcher'> = await build({
+    root: './dist',
+    build: {
+      minify: true,
+      outDir: './distVite',
+      lib: {
+        entry: 'test.js',
+        formats: ['es']
+      }
+    },
+    ...inlineConfig
+  })
+
+
+  const { output } = (Array.isArray(res) ? res[0] : res)
+  
+  for (const chunkOrAsset of output) {
+    if (chunkOrAsset.type === 'chunk') {
+      const res = await gzip(Buffer.from(chunkOrAsset.code))
+
+      filesInfo.push({
+        name: chunkOrAsset.fileName,
+        size: Buffer.byteLength(chunkOrAsset.code) / 1000,
+        gzip: res.length / 1000
+      })
+
+    } else {
+      console.warn("Asset is WIP", Buffer.byteLength(chunkOrAsset.source))
     }
   }
-})
 
-if(Array.isArray(res) && res.length){
-  console.log(res[0]?.output[0].modules)
-}else{
-  console.log(res)
+  return filesInfo
 }
+
+const filesInfo = await buildVite()
+
+console.log(filesInfo)
